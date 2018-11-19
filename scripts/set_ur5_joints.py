709aa3c2c957cd8dd6f309c043e07701d3e0ebe4
Rafael Papallas <rpapallas@gmail.com>
2018-11-19 15:59:08 +0000

    Refactoring; added requirements.txt

--- scripts/set_ur5_joints.py
+++ scripts/set_ur5_joints.py
@@ -18,82 +18,125 @@
 # Author: Rafael Papallas (rpapallas.com)
 
 from pyquaternion import Quaternion
-from xml.etree.ElementTree import XML, SubElement, Element, tostring
+from xml.dom import minidom
 import math
 
-def apply_rotation(q1, axis, angle):
-    angle_in_radians = math.radians(angle)
-    q2 = Quaternion(axis=axis, angle=angle_in_radians)
-    result = q1 * q2
-    return result.elements
+class UR5:
+    def __init__(self, base_joint_value, shoulder_joint_value, elbow_joint_value,
+                 wrist_1_joint_value, wrist_2_joint_value, wrist_3_joint_value):
+        self.base_joint_value = base_joint_value
+        self.base_quat = None
 
-def get_desired_quaternion_for_base_joint_from(angle_in_degrees):
-    original_quaternion = Quaternion(0.681998, 0, 0, -0.731354)
-    desired_quaternion = apply_rotation(original_quaternion,
-                                        axis=[0, 0, 1],
-                                        angle=angle_in_degrees)
-    return desired_quaternion
+        self.shoulder_joint_value = shoulder_joint_value
+        self.shoulder_quat = None
 
-def get_desired_quaternion_for_shoulder_joint_from(angle_in_degrees):
-    original_quaternion = Quaternion(0.707107, 0, 0.707107, 0)
-    desired_quaternion = apply_rotation(original_quaternion,
-                                        axis=[0, 1, 0],
-                                        angle=angle_in_degrees)
-    return desired_quaternion
+        self.elbow_joint_value = elbow_joint_value
+        self.elbow_quat = None
 
-def get_desired_quaternion_for_elbow_joint_from(angle_in_degrees):
-    original_quaternion = Quaternion(1.0, 0, 0, 0)
-    desired_quaternion = apply_rotation(original_quaternion,
-                                        axis=[0, 1, 0],
-                                        angle=angle_in_degrees)
-    return desired_quaternion
+        self.wrist_1_joint_value = wrist_1_joint_value
+        self.wrist_1_quat = None
 
-def get_desired_quaternion_for_wrist1_joint_from(angle_in_degrees):
-    original_quaternion = Quaternion(0.707107, 0, 0.707107, 0)
-    desired_quaternion = apply_rotation(original_quaternion,
-                                        axis=[0, 1, 0],
-                                        angle=angle_in_degrees)
-    return desired_quaternion
+        self.wrist_2_joint_value = wrist_2_joint_value
+        self.wrist_2_quat = None
 
-def get_desired_quaternion_for_wrist2_joint_from(angle_in_degrees):
-    original_quaternion = Quaternion(1.0, 0.0, 0.0, 0.0)
-    desired_quaternion = apply_rotation(original_quaternion,
-                                        axis=[0, 0, 1],
-                                        angle=angle_in_degrees)
-    return desired_quaternion
+        self.wrist_3_joint_value = wrist_3_joint_value
+        self.wrist_3_quat = None
 
-def get_desired_quaternion_for_wrist3_joint_from(angle_in_degrees):
-    original_quaternion = Quaternion(1.0, 0.0, 0.0, 0.0)
-    desired_quaternion = apply_rotation(original_quaternion,
-                                        axis=[0, 1, 0],
-                                        angle=angle_in_degrees)
-    return desired_quaternion
+    def apply_joint_angles(self):
+        self._set_desired_quaternion_for_base_joint()
+        self._set_desired_quaternion_for_shoulder_joint()
+        self._set_desired_quaternion_for_elbow_joint()
+        self._set_desired_quaternion_for_wrist_1_joint()
+        self._set_desired_quaternion_for_wrist_2_joint()
+        self._set_desired_quaternion_for_wrist_3_joint()
 
-def update_xml_with_desired_joint_values(file_path, base_joint_quaternion, shoulder_joint_quaternion, elbow_joint_quaternion, wrist1_joint_quaternion, wrist2_joint_quaternion, wrist3_joint_quaternion):
-    from xml.dom import minidom
-    xmldoc = minidom.parse(file_path)
-    bodies = xmldoc.getElementsByTagName("body")
+    @property
+    def base_quaternion_str(self):
+        return "{} {} {} {}".format(self.base_quat[0], self.base_quat[1],
+                                    self.base_quat[2], self.base_quat[3])
+
+    @property
+    def shoulder_quaternion_str(self):
+        return "{} {} {} {}".format(self.shoulder_quat[0], self.shoulder_quat[1],
+                                    self.shoulder_quat[2], self.shoulder_quat[3])
+
+    @property
+    def elbow_quaternion_str(self):
+        return "{} {} {} {}".format(self.elbow_quat[0], self.elbow_quat[1],
+                                    self.elbow_quat[2], self.elbow_quat[3])
+
+    @property
+    def wrist_1_quaternion_str(self):
+        return "{} {} {} {}".format(self.wrist_1_quat[0], self.wrist_1_quat[1],
+                                    self.wrist_1_quat[2], self.wrist_1_quat[3])
+
+    @property
+    def wrist_2_quaternion_str(self):
+        return "{} {} {} {}".format(self.wrist_2_quat[0], self.wrist_2_quat[1],
+                                    self.wrist_2_quat[2], self.wrist_2_quat[3])
+
+    @property
+    def wrist_3_quaternion_str(self):
+        return "{} {} {} {}".format(self.wrist_3_quat[0], self.wrist_3_quat[1],
+                                    self.wrist_3_quat[2], self.wrist_3_quat[3])
+
+    def _set_desired_quaternion_for_base_joint(self):
+        original_quaternion = Quaternion(0.681998, 0, 0, -0.731354)
+        self.base_quat = self._apply_rotation(original_quaternion, axis=[0, 0, 1], angle=self.base_joint_value)
+
+    def _set_desired_quaternion_for_shoulder_joint(self):
+        original_quaternion = Quaternion(0.707107, 0, 0.707107, 0)
+        self.shoulder_quat = self._apply_rotation(original_quaternion, axis=[0, 1, 0], angle=self.shoulder_joint_value)
+
+    def _set_desired_quaternion_for_elbow_joint(self):
+        original_quaternion = Quaternion(1.0, 0, 0, 0)
+        self.elbow_quat = self._apply_rotation(original_quaternion, axis=[0, 1, 0], angle=self.elbow_joint_value)
+
+    def _set_desired_quaternion_for_wrist_1_joint(self):
+        original_quaternion = Quaternion(0.707107, 0, 0.707107, 0)
+        self.wrist_1_quat = self._apply_rotation(original_quaternion, axis=[0, 1, 0], angle=self.wrist_1_joint_value)
+
+    def _set_desired_quaternion_for_wrist_2_joint(self):
+        original_quaternion = Quaternion(1.0, 0.0, 0.0, 0.0)
+        self.wrist_2_quat = self._apply_rotation(original_quaternion, axis=[0, 0, 1], angle=self.wrist_2_joint_value)
+
+    def _set_desired_quaternion_for_wrist_3_joint(self):
+        original_quaternion = Quaternion(1.0, 0.0, 0.0, 0.0)
+        self.wrist_3_quat = self._apply_rotation(original_quaternion, axis=[0, 1, 0], angle=self.wrist_3_joint_value)
+
+    @staticmethod
+    def _apply_rotation(q1, axis, angle):
+        angle_in_radians = math.radians(angle)
+        q2 = Quaternion(axis=axis, angle=angle_in_radians)
+        result = q1 * q2
+        return result.elements
+
+
+def update_xml_with_desired_joint_values(file_path, ur5):
+    xml_document = minidom.parse(file_path)
+    bodies = xml_document.getElementsByTagName("body")
+
     for body in bodies:
+        if body.attributes["name"].value == "base_link":
+            body.setAttribute("quat", ur5.base_quaternion_str)
+
         if body.attributes["name"].value == "shoulder_link":
-            body.setAttribute("quat", "{} {} {} {}".format(base_joint_quaternion[0], base_joint_quaternion[1], base_joint_quaternion[2], base_joint_quaternion[3]))
+            body.setAttribute("quat", ur5.shoulder_quaternion_str)
 
-        if body.attributes["name"].value == "upper_arm_link":
-            body.setAttribute("quat", "{} {} {} {}".format(shoulder_joint_quaternion[0], shoulder_joint_quaternion[1], shoulder_joint_quaternion[2], shoulder_joint_quaternion[3]))
-
-        if body.attributes["name"].value == "forearm_link":
-            body.setAttribute("quat", "{} {} {} {}".format(elbow_joint_quaternion[0], elbow_joint_quaternion[1], elbow_joint_quaternion[2], elbow_joint_quaternion[3]))
+        if body.attributes["name"].value == "elbow_link":
+            body.setAttribute("quat", ur5.elbow_quaternion_str)
 
         if body.attributes["name"].value == "wrist_1_link":
-            body.setAttribute("quat", "{} {} {} {}".format(wrist1_joint_quaternion[0], wrist1_joint_quaternion[1], wrist1_joint_quaternion[2], wrist1_joint_quaternion[3]))
+            body.setAttribute("quat", ur5.wrist_1_quaternion_str)
 
         if body.attributes["name"].value == "wrist_2_link":
-            body.setAttribute("quat", "{} {} {} {}".format(wrist2_joint_quaternion[0], wrist2_joint_quaternion[1], wrist2_joint_quaternion[2], wrist2_joint_quaternion[3]))
+            body.setAttribute("quat", ur5.wrist_2_quaternion_str)
 
         if body.attributes["name"].value == "wrist_3_link":
-            body.setAttribute("quat", "{} {} {} {}".format(wrist3_joint_quaternion[0], wrist3_joint_quaternion[1], wrist3_joint_quaternion[2], wrist3_joint_quaternion[3]))
+            body.setAttribute("quat", ur5.wrist_3_quaternion_str)
  
     file_writer = open(file_path, "w")
-    xmldoc.writexml(file_writer)
+    xml_document.writexml(file_writer)
 
 
 if __name__ == "__main__":
@@ -104,20 +147,16 @@ if __name__ == "__main__":
     desired_wrist2_joint_angle = float(input("Enter the desired joint angle for the wrist 2 joint in terms of degrees: "))
     desired_wrist3_joint_angle = float(input("Enter the desired joint angle for the wrist 3 joint in terms of degrees: "))
 
-    base_joint_quaternion = get_desired_quaternion_for_base_joint_from(desired_base_joint_angle)
-    shoulder_joint_quaternion = get_desired_quaternion_for_shoulder_joint_from(desired_shoulder_joint_angle)
-    elbow_joint_quaternion = get_desired_quaternion_for_elbow_joint_from(desired_elbow_joint_angle)
-    wrist1_joint_quaternion = get_desired_quaternion_for_wrist1_joint_from(desired_wrist1_joint_angle)
-    wrist2_joint_quaternion = get_desired_quaternion_for_wrist2_joint_from(desired_wrist2_joint_angle)
-    wrist3_joint_quaternion = get_desired_quaternion_for_wrist3_joint_from(desired_wrist3_joint_angle)
-
     model_file_path = input("Please type the path to the xml file to be updated: ")
 
-    update_xml_with_desired_joint_values(model_file_path,
-                                         base_joint_quaternion,
-                                         shoulder_joint_quaternion,
-                                         elbow_joint_quaternion,
-                                         wrist1_joint_quaternion,
-                                         wrist2_joint_quaternion,
-                                         wrist3_joint_quaternion)
+    ur5 = UR5(desired_base_joint_angle, desired_shoulder_joint_angle,
+              desired_elbow_joint_angle, desired_wrist1_joint_angle,
+              desired_wrist2_joint_angle, desired_wrist3_joint_angle)
+
+    # Do the required calculations to convert the desired joint angles into
+    # correct quaternions to set the MuJoCo model.
+    ur5.apply_joint_angles()
+
+    # Now update the model file on disk.
+    update_xml_with_desired_joint_values(model_file_path, ur5)
 
